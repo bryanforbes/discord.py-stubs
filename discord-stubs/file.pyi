35adc1076da25cab98858bbab8fb051e3c679863
Faholan <62927863+Faholan@users.noreply.github.com>
2021-04-27 17:30:19 +0200

    Add typehints to File class

--- discord-stubs/file.pyi
+++ discord-stubs/file.pyi
@@ -1,12 +1,13 @@
 from typing import BinaryIO, Optional, Union
+from io import BytesIO, StringIO
 
 class File:
-    fp: Union[str, BinaryIO]
+    fp: Union[str, BinaryIO, BytesIO, StringIO]
     filename: Optional[str]
     spoiler: bool
     def __init__(
         self,
-        fp: Union[str, BinaryIO],
+        fp: Union[str, BinaryIO, BytesIO, StringIO],
         filename: Optional[str] = ...,
         *,
         spoiler: bool = ...,
