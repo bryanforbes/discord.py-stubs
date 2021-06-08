e7f2b7b46bfc68c3cd976f22a7529a7182e7b12e
Bryan Forbes <bryan@reigndropsfall.net>
2021-06-08 14:06:09 -0500

    Updates for mypy 0.900

--- discord-stubs/object.pyi
+++ discord-stubs/object.pyi
@@ -1,11 +1,11 @@
 import datetime
-from builtins import _SupportsIndex
 from typing import SupportsInt, Text, Union
+from typing_extensions import SupportsIndex
 
 from .mixins import Hashable
 
 class Object(Hashable):
     id: int
-    def __init__(self, id: Union[Text, bytes, SupportsInt, _SupportsIndex]) -> None: ...
+    def __init__(self, id: Union[Text, bytes, SupportsInt, SupportsIndex]) -> None: ...
     @property
     def created_at(self) -> datetime.datetime: ...
